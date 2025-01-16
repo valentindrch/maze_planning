4965b20b555802ba84da3daa4f09847990f4cbc2
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-01-16 19:01:15 +0100

    finalize sampling alg plot

--- plot_sampling_alg.py
+++ plot_sampling_alg.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pickle
 import numpy as np
+import pandas as pd
 from pgmpy.factors.discrete import TabularCPD
 
 from sampling import Sampler
@@ -31,18 +32,38 @@ def def_goal(index, value=.95):
     model.add_cpds(cpd_o3_given_s3)
 
 # Sample for different goals
-def_goal(4)
+data = {'goal': [], 't': [], 'i': [], 'n_samples': []}
+for goal in range(8):
+    print(goal)
 
-n_samples = []
-for i in [0, 5, 50]:
-    print(i)
+    def_goal(goal)
+    for i in [0, 5, 50]:
+        for j in range(20):
+            sampling_alg = Sampler(model, priors[i])
+            sampled_posterior, n = sampling_alg.query(None, evidence={'o3': 0})
+            
+            data['goal'].append(goal)
+            data['t'].append(i)
+            data['i'].append(j)
+            data['n_samples'].append(n)
 
-    n_samples_i = []
-    for j in range(10):
-        sampling_alg = Sampler(model, priors[i])
-        sampled_posterior, n = sampling_alg.query(None, evidence={'o3': 0})
-        n_samples_i.append(n)
+data = pd.DataFrame(data)
 
-    n_samples.append(n_samples_i)
+# Create figure with three subplots
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+
+# Plot for each t
+for t, ax in zip([0, 5, 50], [ax1, ax2, ax3]):
+    # Get data for this t and calculate mean
+    t_data = data[data['t'] == t].groupby('goal')['n_samples'].mean()
+    
+    # Create bar plot
+    ax.bar(t_data.index, t_data.values)
+    ax.set_xlabel('Goal')
+    ax.set_ylabel('Number of samples')
+    ax.set_title(f't = {t}')
+
+plt.tight_layout()
+plt.show()
 
 a = 1
